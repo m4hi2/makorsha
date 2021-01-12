c5f9d5f9fb358552b6b38c1847824a446c114be2
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-01-12 12:16:07 +0000

    feat: removed caching
    Caching was taking too much space and causing the program to crash
    randomly due to not being able to save the cache files.

--- corpus_builder/corpus_builder/settings.py
+++ corpus_builder/corpus_builder/settings.py
@@ -81,8 +81,8 @@ CONCURRENT_REQUESTS_PER_IP = 16
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 86400
-HTTPCACHE_DIR = 'httpcache'
-HTTPCACHE_IGNORE_HTTP_CODES = []
-HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 86400
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
